6b25317f7dfb614b03aff0ae195aef61583e9142
LucasFrancaRegistro <lucascumuru@gmail.com>
2023-03-16 20:14:50 -0300

    teste de conexao com o banco

--- neobrasil-backend/src/main/java/com/fatec/aplicacao/AplicacaoApplication.java
+++ neobrasil-backend/src/main/java/com/fatec/aplicacao/AplicacaoApplication.java
@@ -1,5 +1,9 @@
 package com.fatec.aplicacao;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AplicacaoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AplicacaoApplication.class, args);
+		/*
+		 * Código padrão do spring, comentado
+		 * SpringApplication.run(BancoApplication.class, args);
+		 */
+
+		Map<String, Object> configuracao = new HashMap<>();
+
+		configuracao.put("server.port", "9080"); 
+		configuracao.put("spring.datasource.url", "jdbc:mysql://localhost:3306/banco"); 
+		
+		configuracao.put("spring.datasource.username", "root"); 
+		configuracao.put("spring.datasource.password", "root"); 
+		
+		configuracao.put("spring.jpa.show-sql", "true"); 
+		configuracao.put("spring.jpa.hibernate.ddl-auto", "update");
+		
+		SpringApplication app = new SpringApplication(AplicacaoApplication.class);
+		app.setDefaultProperties(configuracao);
+		app.run(args);
 	}
 
 }
