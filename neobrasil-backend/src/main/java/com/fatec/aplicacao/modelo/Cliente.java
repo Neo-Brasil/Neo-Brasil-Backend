d97b929cdb6810afc3b25ba680df92ae06b52572
LucasFrancaRegistro <lucascumuru@gmail.com>
2023-03-20 21:00:10 -0300

    tentativa de chave estrangeira

--- neobrasil-backend/src/main/java/com/fatec/aplicacao/modelo/Cliente.java
+++ neobrasil-backend/src/main/java/com/fatec/aplicacao/modelo/Cliente.java
@@ -1,10 +1,13 @@
 package com.fatec.aplicacao.modelo;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
 
 @Data
@@ -12,6 +15,7 @@ import lombok.Data;
 public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "cliente_id")
 	private Long id;
 	@Column
 	private String nome;
@@ -19,5 +23,6 @@ public class Cliente {
 	private String cpf;
 	@Column
 	private String email;
-	
+	@OneToOne(mappedBy = "cliente", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private Endereço endereco;
 }
