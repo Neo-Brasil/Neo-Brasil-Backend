f9076b404d3ec2edda73d75c157d6eacd0b076df
Desduh <eduardofalandess@gmail.com>
2023-03-29 18:08:52 -0300

    remocao de comentarios

--- neobrasil-backend/src/main/java/com/fatec/aplicacao/controle/ControleTitulos.java
+++ neobrasil-backend/src/main/java/com/fatec/aplicacao/controle/ControleTitulos.java
@@ -2,20 +2,24 @@ package com.fatec.aplicacao.controle;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 //import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fatec.aplicacao.modelo.Titulos;
 import com.fatec.aplicacao.recursos.Selecionador;
+import com.fatec.aplicacao.recursos.TituloAtualizador;
 //import com.fatec.aplicacao.recursos.TituloAtualizador;
 import com.fatec.aplicacao.repositorio.RepositorioTitulos;
 
 @RestController
+@CrossOrigin
 public class ControleTitulos {
 	
 	@Autowired
@@ -35,14 +39,14 @@ public class ControleTitulos {
 		List<Titulos> titulos = repositorio.findAll();
 		return Selecionador.selecionarTitulos(titulos, id);
 	}
-//	@SuppressWarnings("deprecation")
-//	@PutMapping("/atualizar/titulo")
-//	public void atualizarCliente(@RequestBody Titulos atualizacao) {
-//		Titulos titulo = repositorio.getById(atualizacao.getId());
-//		TituloAtualizador atualizador = new TituloAtualizador();
-//		atualizador.atualizar(titulo, atualizacao);
-//		repositorio.save(titulo);
-//	}
+	@SuppressWarnings("deprecation")
+	@PutMapping("/atualizar/titulo")
+	public void atualizarCliente(@RequestBody Titulos atualizacao) {
+		Titulos titulo = repositorio.getById(atualizacao.getId());
+		TituloAtualizador atualizador = new TituloAtualizador();
+		atualizador.atualizar(titulo, atualizacao);
+		repositorio.save(titulo);
+	}
 	@SuppressWarnings("deprecation")
 	@DeleteMapping("/excluir/titulos")
 	public void excluirCliente(@RequestBody Titulos exclusao) {
