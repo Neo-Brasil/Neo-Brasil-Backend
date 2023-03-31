ad3abfffc3f7163e35566b7b47dfd8ff3eec4c88
LucasFrancaRegistro <lucascumuru@gmail.com>
2023-03-30 21:45:30 -0300

    rota de verificacao e atualizacao da situacao do pagamento das prestacoes

--- neobrasil-backend/src/main/java/com/fatec/aplicacao/controle/ControleTitulos.java
+++ neobrasil-backend/src/main/java/com/fatec/aplicacao/controle/ControleTitulos.java
@@ -11,10 +11,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 //import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+import com.fatec.aplicacao.modelo.Cliente;
 import com.fatec.aplicacao.modelo.Titulos;
 import com.fatec.aplicacao.recursos.Selecionador;
 import com.fatec.aplicacao.recursos.TituloAtualizador;
+import com.fatec.aplicacao.repositorio.RepositorioCliente;
 //import com.fatec.aplicacao.recursos.TituloAtualizador;
 import com.fatec.aplicacao.repositorio.RepositorioTitulos;
 
@@ -23,34 +27,56 @@ import com.fatec.aplicacao.repositorio.RepositorioTitulos;
 public class ControleTitulos {
 	
 	@Autowired
-	private RepositorioTitulos repositorio;
+	private RepositorioTitulos repositorioTitulos;
+	
+	@Autowired
+	private RepositorioCliente repositorioCliente;
 	
 	@PostMapping("/cadastro/titulos")
 	public void cadastrar(@RequestBody Titulos novoTitulos) {
-		repositorio.save(novoTitulos);
+		repositorioTitulos.save(novoTitulos);
 	}
+	@GetMapping("/listagem/titulos/atualizar_situacao")
+	public List<Cliente> obterTitulosAtualizarSituacao(){
+		List<Cliente> clientes = repositorioCliente.findAll();
+		int data_atual = Integer.parseInt(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd")).replace("/", ""));
+		for (Cliente cliente : clientes ) {
+			List<Titulos> titulos = cliente.getTitulos();
+			for (Titulos titulo : titulos) {
+				if ( !titulo.getSituacao().equalsIgnoreCase("Pago")) {
+					int data_vencimento = Integer.parseInt(titulo.getData_vencimento().replace("/", ""));
+					if (data_vencimento < data_atual) {
+						titulo.setSituacao("Atrasado");
+						}
+					}
+				}
+			}
+		return clientes;
+		}
+	
 	@GetMapping("/listagem/titulos")
-	public List<Titulos> obterUsuario(){
-		List<Titulos> titulos = repositorio.findAll();
+	public List<Titulos> obterListaTitulos(){
+		List<Titulos> titulos = repositorioTitulos.findAll();
 		return titulos;
 	}
+	
 	@GetMapping("/selecionar/titulos/{id}")
 	public Titulos obterTitulos(@PathVariable long id) {
-		List<Titulos> titulos = repositorio.findAll();
+		List<Titulos> titulos = repositorioTitulos.findAll();
 		return Selecionador.selecionarTitulos(titulos, id);
 	}
 	@SuppressWarnings("deprecation")
 	@PutMapping("/atualizar/titulo")
 	public void atualizarCliente(@RequestBody Titulos atualizacao) {
-		Titulos titulo = repositorio.getById(atualizacao.getId());
+		Titulos titulo = repositorioTitulos.getById(atualizacao.getId());
 		TituloAtualizador atualizador = new TituloAtualizador();
 		atualizador.atualizar(titulo, atualizacao);
-		repositorio.save(titulo);
+		repositorioTitulos.save(titulo);
 	}
 	@SuppressWarnings("deprecation")
 	@DeleteMapping("/excluir/titulos")
 	public void excluirCliente(@RequestBody Titulos exclusao) {
-		Titulos titulos = repositorio.getById(exclusao.getId());
-		repositorio.delete(titulos);
+		Titulos titulos = repositorioTitulos.getById(exclusao.getId());
+		repositorioTitulos.delete(titulos);
 	}
 }
