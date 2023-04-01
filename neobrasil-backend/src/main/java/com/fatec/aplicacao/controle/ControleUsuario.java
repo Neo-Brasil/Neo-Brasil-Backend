4697f811e697e47eb066d3cf5fbb87a589153335
Desduh <eduardofalandess@gmail.com>
2023-04-01 19:48:07 -0300

    correcao de problema com o cors

--- neobrasil-backend/src/main/java/com/fatec/aplicacao/controle/ControleUsuario.java
+++ neobrasil-backend/src/main/java/com/fatec/aplicacao/controle/ControleUsuario.java
@@ -2,6 +2,7 @@ package com.fatec.aplicacao.controle;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import com.fatec.aplicacao.recursos.Selecionador;
 import com.fatec.aplicacao.repositorio.RepositorioUsuario;
 
 @RestController
+@CrossOrigin
 public class ControleUsuario {
 	
 	@Autowired
