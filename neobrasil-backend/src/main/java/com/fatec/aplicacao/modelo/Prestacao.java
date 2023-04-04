a32eb7691fa997923a0aeba2a41ee079a27def83
LucasFrancaRegistro <lucascumuru@gmail.com>
2023-04-03 23:13:25 -0300

    criacao da classe/tabel prestacao com chave estrangeira

---
+++ neobrasil-backend/src/main/java/com/fatec/aplicacao/modelo/Prestacao.java
@@ -0,0 +1,31 @@
+package com.fatec.aplicacao.modelo;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Entity
+@Getter
+@Setter
+public class Prestacao {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long Id;
+	@Column
+	private String data_vencimento;
+	@Column
+	private String data_pagamento;
+	@Column
+	private int preco;
+	
+}
