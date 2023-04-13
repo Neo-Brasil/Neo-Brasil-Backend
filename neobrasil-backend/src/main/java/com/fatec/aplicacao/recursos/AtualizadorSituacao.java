8f9867468355d16fc4a020116a2c354e6f0d1a76
LucasFrancaRegistro <lucascumuru@gmail.com>
2023-04-13 20:08:51 -0300

    correcao de atualizacao de prestacoes em aberto

--- neobrasil-backend/src/main/java/com/fatec/aplicacao/recursos/AtualizadorSituacao.java
+++ neobrasil-backend/src/main/java/com/fatec/aplicacao/recursos/AtualizadorSituacao.java
@@ -8,14 +8,18 @@ public class AtualizadorSituacao {
 	public static void atualizarTituloPrestacoes(Titulos titulo, int dataAtual) {
 		for (Prestacao prestacao: titulo.getPrestacoes()) {
 			if ( !prestacao.getSituacao().equalsIgnoreCase("Pago")) {
-				int data_vencimento = Integer.parseInt(titulo.getData_vencimento().replace("-", ""));
+				int data_vencimento = Integer.parseInt(prestacao.getData_vencimento().replace("-", ""));
 				if (data_vencimento <= dataAtual) {
+					System.out.print(data_vencimento);
+					System.out.print("/");
+					System.out.print(dataAtual);
+					System.out.print("@");
 					prestacao.setSituacao("Inadimplente");
 					}
-				} else if (
-					Integer.parseInt(prestacao.getData_pagamento().replace("-", "")) +
-					titulo.getTempo_credito() <= dataAtual) {
-					prestacao.setSituacao("Creditado");
+			} else if (
+				Integer.parseInt(prestacao.getData_pagamento().replace("-", "")) +
+				titulo.getTempo_credito() <= dataAtual) {
+				prestacao.setSituacao("Creditado");
 				}
 			}
 		}
