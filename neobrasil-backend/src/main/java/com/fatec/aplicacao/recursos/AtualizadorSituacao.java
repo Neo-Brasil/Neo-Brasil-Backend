bd411894347b33412614fb3d0a2183b8a0e374b2
LucasFrancaRegistro <lucascumuru@gmail.com>
2023-04-12 22:02:49 -0300

    funcao atualizar prestacoes

---
+++ neobrasil-backend/src/main/java/com/fatec/aplicacao/recursos/AtualizadorSituacao.java
@@ -0,0 +1,27 @@
+package com.fatec.aplicacao.recursos;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.fatec.aplicacao.modelo.Prestacao;
+import com.fatec.aplicacao.modelo.Titulos;
+
+public class AtualizadorSituacao {
+	
+	public static void atualizarTituloPrestacoes(Titulos titulo, int dataAtual) {
+		for (Prestacao prestacao: titulo.getPrestacoes()) {
+			if ( !prestacao.getSituacao().equalsIgnoreCase("Pago")) {
+				int data_vencimento = Integer.parseInt(titulo.getData_vencimento().replace("-", ""));
+				if (data_vencimento <= dataAtual) {
+					prestacao.setSituacao("Inadimplente");
+					}
+				} else if (
+					Integer.parseInt(prestacao.getData_pagamento().replace("-", "")) +
+					titulo.getTempo_credito() <= dataAtual) {
+					prestacao.setSituacao("Creditado");
+				}
+			}
+		}
+	}
+
