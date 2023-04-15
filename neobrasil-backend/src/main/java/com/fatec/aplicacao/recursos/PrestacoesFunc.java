0f1024e41ec3b8c4abe8e860f2dbfbab7c627f8e
LucasFrancaRegistro <lucascumuru@gmail.com>
2023-04-14 21:28:28 -0300

    criacao de funcao CriarParcela

--- neobrasil-backend/src/main/java/com/fatec/aplicacao/recursos/PrestacoesFunc.java
+++ neobrasil-backend/src/main/java/com/fatec/aplicacao/recursos/PrestacoesFunc.java
@@ -1,19 +1,19 @@
 package com.fatec.aplicacao.recursos;
 
+import java.text.ParseException;
+import java.util.List;
+
 import com.fatec.aplicacao.modelo.Prestacao;
 import com.fatec.aplicacao.modelo.Titulos;
 
-public class AtualizadorSituacao {
+
+public class PrestacoesFunc {
 	
 	public static void atualizarTituloPrestacoes(Titulos titulo, int dataAtual) {
 		for (Prestacao prestacao: titulo.getPrestacoes()) {
 			if ( !prestacao.getSituacao().equalsIgnoreCase("Pago")) {
 				int data_vencimento = Integer.parseInt(prestacao.getData_vencimento().replace("-", ""));
 				if (data_vencimento <= dataAtual) {
-					System.out.print(data_vencimento);
-					System.out.print("/");
-					System.out.print(dataAtual);
-					System.out.print("@");
 					prestacao.setSituacao("Inadimplente");
 					}
 			} else if (
@@ -22,6 +22,17 @@ public class AtualizadorSituacao {
 				prestacao.setSituacao("Creditado");
 				}
 			}
+		}
+		
+		public static Titulos criarNovaPrestacao(Titulos titulo) throws ParseException {
+			Prestacao novaPrestacao = new Prestacao();
+			List<Prestacao> prestacoes = titulo.getPrestacoes();
+			Prestacao ultimaPrestacao = prestacoes.get(prestacoes.size() - 1);
+			novaPrestacao.setData_vencimento(DataManipulacao.AdicionarDias(ultimaPrestacao.getData_vencimento(), 30));
+			novaPrestacao.setPreco(ultimaPrestacao.getPreco());
+			novaPrestacao.setSituacao("Em aberto");
+			prestacoes.add(novaPrestacao);
+			return titulo;
 		}
 	}
 
