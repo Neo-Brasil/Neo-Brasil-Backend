53dbce05af6acae55b9cb03a851b44cd8726ae7f
LucasFrancaRegistro <lucascumuru@gmail.com>
2023-04-19 20:39:14 -0300

    adicao de get e setters

--- neobrasil-backend/src/main/java/com/fatec/aplicacao/modelo/RelatorioValores.java
+++ neobrasil-backend/src/main/java/com/fatec/aplicacao/modelo/RelatorioValores.java
@@ -1,10 +1,33 @@
 package com.fatec.aplicacao.modelo;
 
-@SuppressWarnings("unused")
 public class RelatorioValores {
 	private float expectativa;
 	
 	private float faltante;
 	
 	private float recebido;
+
+	public float getExpectativa() {
+		return expectativa;
+	}
+
+	public void setExpectativa(float expectativa) {
+		this.expectativa = expectativa;
+	}
+
+	public float getFaltante() {
+		return faltante;
+	}
+
+	public void setFaltante(float faltante) {
+		this.faltante = faltante;
+	}
+
+	public float getRecebido() {
+		return recebido;
+	}
+
+	public void setRecebido(float recebido) {
+		this.recebido = recebido;
+	}
 }
